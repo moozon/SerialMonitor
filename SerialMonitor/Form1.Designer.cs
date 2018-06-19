cd1918565958758006a94c9755e5ffff475b65e3
Mikhail Kharitonov <Moozon@mail.ru>
2018-06-19 21:12:11 +0500

    Добавьте файлы проекта.

---
+++ SerialMonitor/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(622, 154);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(41, 23);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Clear";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 160);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(21, 76);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PinChanged += new System.IO.Ports.SerialPinChangedEventHandler(this.serialPort1_PinChanged);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(102, 78);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(53, 21);
+            this.buttonRefresh.TabIndex = 5;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(21, 9);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(134, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxOut.Location = new System.Drawing.Point(189, 49);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut.Size = new System.Drawing.Size(474, 99);
+            this.textBoxOut.TabIndex = 7;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(622, 7);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(41, 23);
+            this.buttonSend.TabIndex = 8;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(189, 9);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(427, 20);
+            this.textBoxSend.TabIndex = 9;
+            this.textBoxSend.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxSend_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 189);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "Form1";
+            this.Text = "SerialMonitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxSend;
+    }
+}
+
---
+++ SerialMonitor/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialMonitor
+{
+
+    public partial class Form1 : Form
+    {
+        int[] baudRate = { 9600, 115200 };
+        string[] comPorts;
+        //string buf;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (checkBox1.Checked == false) textBox1.Enabled = false;
+            comPorts = System.IO.Ports.SerialPort.GetPortNames();
+            Array.Sort(comPorts);
+            comboBox1.DataSource = comPorts;
+            comboBox2.DataSource = baudRate;
+            buttonSend.Enabled = false;
+            //buttonClose.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxOut.Clear();
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) textBox1.Enabled = true;
+            else textBox1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {           
+            comPorts = System.IO.Ports.SerialPort.GetPortNames();
+            Array.Sort(comPorts);
+            comboBox1.DataSource = comPorts;
+            if (comboBox1.Items.Count == 0) comboBox1.Text = "";
+            else comboBox1.Text = comboBox1.Items[0].ToString();
+            comboBox1.Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonConnect.Text = "Connect";
+                this.Text = "SerialMonitor";
+                buttonSend.Enabled = false;
+                buttonRefresh.Enabled = true;
+                textBoxOut.AppendText("Close Connect" + Environment.NewLine);
+            }
+            else
+            {                
+                if (comboBox1.Items.Count > 0) serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                else
+                {
+                    textBoxOut.AppendText("Выберите порт" + Environment.NewLine);
+                    return;
+                }
+
+                try
+                {
+                    serialPort1.BaudRate = (int)comboBox2.SelectedItem;
+                }
+                catch (Exception)
+                {
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                }
+
+
+                try
+                {
+                    serialPort1.Open();
+                    buttonConnect.Text = "Disconnect";
+                    this.Text += " " + comboBox1.SelectedItem.ToString();
+                    textBoxOut.AppendText("Connect succesfully" + Environment.NewLine);
+                    //buttonConnect.Enabled = false;
+                    buttonRefresh.Enabled = false;
+                    buttonSend.Enabled = true;
+                    //buttonClose.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    textBoxOut.AppendText("No Connect" + Environment.NewLine);
+                }
+
+            }
+        }
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            this.Text = "SerialMonitor";
+            //buttonClose.Enabled = false;
+            buttonConnect.Enabled = true;
+            buttonSend.Enabled = false;
+            textBoxOut.AppendText("Close Connect" + Environment.NewLine);            
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Write(textBoxSend.Text);
+            else textBoxOut.AppendText("Соединение разорвано" + Environment.NewLine);
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+                textBoxOut.Invoke((MethodInvoker)(() => textBoxOut.AppendText(serialPort1.ReadExisting())));            
+        }
+
+        private void serialPort1_PinChanged(object sender, System.IO.Ports.SerialPinChangedEventArgs e)
+        {
+            textBoxOut.AppendText("Соединение разорвано" + Environment.NewLine);
+        }        
+
+        private void textBoxSend_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) buttonSend_Click(new object(), new EventArgs());
+        }
+    }
+}
